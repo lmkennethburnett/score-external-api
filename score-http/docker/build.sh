2d28786a3029ea03fb4457ba0feba94a3a420578
Hakju Oh <hakju.oh@gmail.com>
2025-08-05 15:37:08 -0400

    Update version properties to 3.4.1

--- score-http/docker/build.sh
+++ score-http/docker/build.sh
@@ -13,15 +13,15 @@ cd ..
 ./mvnw clean package -DskipTests=true
 
 echo "Preparing files..."
-cp target/score-http-3.4.0.1.war docker
-cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.5.3/mariadb-java-client-3.5.3.jar docker
+cp target/score-http-3.4.1.war docker
+cp ~/.m2/repository/org/mariadb/jdbc/mariadb-java-client/3.5.4/mariadb-java-client-3.5.4.jar docker
 
 echo "Building docker image..."
 cd docker
-docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.4.0.1 .
+docker build --no-cache -f Dockerfile -t oagi1docker/srt-http-gateway:3.4.1 .
 
 echo "Scanning vulnerabilities..."
-docker scout cves oagi1docker/srt-http-gateway:3.4.0.1
+docker scout cves oagi1docker/srt-http-gateway:3.4.1
 
 echo "Cleaning up..."
 rm -f *.jar *.war
